5bf9db7e2329fd333dd886399996b08a873d0c86
Random9904 <alagunajr@gmail.com>
2023-12-07 17:23:45 -0600

    Added challenge8

---
+++ Ops Challenge/challenge8.py
@@ -0,0 +1,51 @@
+# Sample values
+a = 5
+b = 10
+
+# Equals
+if a == b:
+    print("a is equal to b")
+
+# Not Equals
+if a != b:
+    print("a is not equal to b")
+
+# Less than
+if a < b:
+    print("a is less than b")
+
+# Less than or equal to
+if a <= b:
+    print("a is less than or equal to b")
+
+# Greater than
+if a > b:
+    print("a is greater than b")
+
+# Greater than or equal to
+if a >= b:
+    print("a is greater than or equal to b")
+
+# Sample value 
+value = 7
+
+# Greater than 10
+if value > 10:
+    print("value is greater than 10")
+# Greater than 5 but less than  10
+elif value > 5:
+    print("value is greater than 5 but not greater than 10")
+
+
+# Sample value
+x = 30
+
+# Greater than 20
+if x > 20:
+    print("x is greater than 20")
+# Greater than 10 but not 20
+elif x > 10:
+    print("x is greater than 10 but not greater than 20")
+# 10 or less
+else:
+    print("x is 10 or less")
